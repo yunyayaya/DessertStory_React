efc2d7f0c8b59dbb78c0c62b67c4631dff22038a
yuying <yuying9219@gmail.com>
2025-02-14 19:43:32 +0800

    環境建立完成

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef, useState } from 'react'
+import axios from 'axios'
 import './App.css'
+import { Modal } from 'bootstrap'
+const baseUrl = import.meta.env.VITE_BASE_URL;
+const apiPath = import.meta.env.VITE_API_PATH;
+console.log(baseUrl);
+console.log(apiPath);
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get('https://randomuser.me/api/');
+      console.log(res);
+
+    })()
+  }, [])
+
+  //導入modal功能
+  const modalRef =useRef(null);
+
+  useEffect(()=>{
+    new Modal (modalRef.current);
+  },[])
+
+  const handleModalShow = ()=>{
+    const modalInstance =Modal.getInstance(modalRef.current);
+    modalInstance.show()
+  }
+
+  const handleModalHide = ()=>{
+    const modalInstance =Modal.getInstance(modalRef.current);
+    modalInstance.hide()
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
+      <button type="button" className="btn btn-primary" onClick={handleModalShow}>
+        Launch demo modal
+      </button>
+
+      <div className="modal fade" ref={modalRef} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={handleModalHide}></button>
+            </div>
+            <div className="modal-body">
+              ...
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={handleModalHide}>Close</button>
+              <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
