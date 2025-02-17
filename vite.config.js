b15517331ac51b0235fc926b0ffaafb6a8168ac3
Yunya <alice504119@gmail.com>
2025-02-17 22:42:07 +0800

    建立gh-page

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //開發中、產品路徑
+  base: process.env.Node_ENV === 'production' ? '/DessertStory_React/' : '/',
   plugins: [react()],
 })
